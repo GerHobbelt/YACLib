6352e76f170f39f4c6cd1751f76ad854fe6f05d6
Maria Filipanova <myannyax@gmail.com>
2022-05-24 15:46:55 +0000

    smol things

--- include/yaclib/fault/detail/fiber/thread_local_proxy.hpp
+++ include/yaclib/fault/detail/fiber/thread_local_proxy.hpp
@@ -13,25 +13,42 @@ static uint64_t nextFreeIndex{0};
 template <typename Type>
 class ThreadLocalPtrProxy {
  public:
+  ThreadLocalPtrProxy() : _i(nextFreeIndex++) {
+    defaults[_i] = nullptr;
+  }
+
   ThreadLocalPtrProxy(Type* value) : _i(nextFreeIndex++) {
-    defaults[nextFreeIndex] = value;
+    defaults[_i] = value;
   }
 
   ThreadLocalPtrProxy& operator=(Type* value) {
     auto* fiber = fault::Scheduler::Current();
-    fiber->SetTls(nextFreeIndex, value);
+    fiber->SetTls(_i, value);
     return *this;
   }
 
-  Type& operator*() {
+  Type& operator*() const noexcept {
     auto* fiber = fault::Scheduler::Current();
-    auto& val = *(static_cast<Type*>(fiber->GetTls(nextFreeIndex, defaults[nextFreeIndex])));
+    auto& val = *(static_cast<Type*>(fiber->GetTls(_i, defaults[_i])));
     return val;
   }
 
-  Type* operator->() {
+  Type* operator->() const noexcept {
     auto* fiber = fault::Scheduler::Current();
-    return static_cast<Type*>(fiber->GetTls(nextFreeIndex, defaults[nextFreeIndex]));
+    return static_cast<Type*>(fiber->GetTls(_i, defaults[_i]));
+  }
+
+  explicit operator bool() const noexcept {
+    auto* fiber = fault::Scheduler::Current();
+    auto* val = static_cast<Type*>(fiber->GetTls(_i, defaults[_i]));
+    return val != nullptr;
+  }
+
+  Type& operator[](std::size_t i) const {
+    auto* fiber = fault::Scheduler::Current();
+    auto* val = static_cast<Type*>(fiber->GetTls(_i, defaults[_i]));
+    val += i;
+    return *val;
   }
 
  private:
